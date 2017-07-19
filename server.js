1384c1602ae63b50ce9c5c9e03e0357290c87a81
Daniel Gil <t-dagil@microsoft.com>
2017-07-19 18:55:19 -0400

    Socket connection working, brings players to a lobby

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+users = [];
 connections = [];
+connectionToUser = {};
 
 app.set('port', process.env.PORT || 5000);
 server.listen(app.get('port'), function() {
@@ -12,4 +14,23 @@ server.listen(app.get('port'), function() {
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	socket.on('add user', function(username) {
+		connectionToUser[socket] = username;
+		users.push(username);
+		io.sockets.emit('update users', {msg: users});
+	});
+
+	socket.on('disconnect', function(data) {
+		users.splice(users.indexOf(socketToUsername[socket]), 1);
+		io.sockets.emit('update users', {msg: users});
+
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
 });
