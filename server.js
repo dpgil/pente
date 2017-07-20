545a5da6fe6e0e2a577b77579f10b2f23cdf2215
Daniel Gil <t-dagil@microsoft.com>
2017-07-20 14:29:16 -0400

    Made more logical game matching where the second player won't even join the lobby if there is already someone waiting for a game. Simply pulls the opponent from the lobby and creates a new game

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-var unpairedConnections = []; // socket ids of players not yet assigned to a game
-var socketIdToUsername = {};
-var lastGameId = 0; // most recent game id assigned
+var unpairedConnections = []; // socket objects of players not yet assigned to a game
+var socketIdToUsername = {}; // maps socket ids to player username
 var socketIdToGameId = {}; // maps player socket ids to game
+var lastGameId = 0; // most recent game id assigned
 
 app.set('port', process.env.PORT || 5000);
 server.listen(app.get('port'), function() {
@@ -18,16 +18,20 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
-	unpairedConnections.push(socket);
-	console.log('Connected: %s sockets connected', unpairedConnections.length);
+	console.log('Socket connected');
 
-	socket.join('lobby');
-
-	socket.on('add user', function(username) {
+	// Called when the user submits a username and intends to join the lobby
+	socket.on('join lobby', function(username) {
 		socketIdToUsername[socket.id] = username;
-		tryPairUser();
+
+		// Couldn't find a match, join the lobby and wait
+		if (!tryPairUser()) {
+			unpairedConnections.push(socket);
+		}
 	});
 
+	// Called when a player disconnects. Either removes them from
+	// available matches or notifies their opponent they forfeited
 	socket.on('disconnect', function(data) {
 		if (contains(unpairedConnections, socket)) {
 			// Player was in the lobby, no longer available to pair
@@ -43,54 +47,46 @@ io.sockets.on('connection', function(socket) {
 		console.log('Socket disconnected');
 	});
 
+	// Array contains helper function
 	function contains(array, item) {
 		return array.indexOf(item) >= 0;
 	}
 
+	// Assigns a new game id
 	function getGameId() {
 		lastGameId = lastGameId + 1;
 		return lastGameId;
 	}
 
-	// Matches two players in a game
-	function matchPlayers(player1, player2) {
-		// Removes them both from out unpaired list
-		unpairedConnections.splice(unpairedConnections.indexOf(player1), 1);
-		unpairedConnections.splice(unpairedConnections.indexOf(player2), 1);
-
-		// Leave the lobby room
-		player1.leave('lobby');
-		player2.leave('lobby');
+	// Matches current player with opponent who was waiting in the lobby
+	function matchPlayer(opponent) {
+		// Removes opponent from unpaired list
+		unpairedConnections.splice(unpairedConnections.indexOf(opponent), 1);
 
 		// Assigns a game id
 		var gameId = getGameId();
 
-		// Adds them to our game to player map
-		socketIdToGameId[player1.id] = gameId;
-		socketIdToGameId[player2.id] = gameId;
+		// Adds them to our player map
+		socketIdToGameId[socket.id] = gameId;
+		socketIdToGameId[opponent.id] = gameId;
 
 		// Join the game room
-		player1.join(gameId);
-		player2.join(gameId);
+		socket.join(gameId);
+		opponent.join(gameId);
 
-		// Tells each player we found a match and their opponents username
-		player1.emit('found match', {msg: socketIdToUsername[player2.id]});
-		player2.emit('found match', {msg: socketIdToUsername[player1.id]});
-
-		console.log('Paired players '+socketIdToUsername[player1.id]+' and '+socketIdToUsername[player2.id]);
-
+		// Tells each player we found a match and their opponent's username
+		socket.emit('found match', {msg: socketIdToUsername[opponent.id]});
+		opponent.emit('found match', {msg: socketIdToUsername[socket.id]});
 	}
 
 	// Tries to find a game match for the user that just connected
 	function tryPairUser() {
-		for (i = 0; i < unpairedConnections.length; i++) {
-			// Found a player that is not itself
-			if (unpairedConnections[i].id !== socket.id) {
-				matchPlayers(socket, unpairedConnections[i]);
-				return true;
-			}
+		// If someone is waiting for a match, we have an opponent
+		if (unpairedConnections.length > 0) {
+			matchPlayer(unpairedConnections[0]);
+			return true;
+		} else {
+			return false;
 		}
-
-		return false;
 	}
 });
